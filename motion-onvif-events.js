832fee0e9032946c360d813a4b8feb22130f10bf
rostislavzz <rost.shikhov@gmail.com>
2021-02-13 14:03:38 +0300

    Refactor, add prettier

--- motion-onvif-events.js
+++ motion-onvif-events.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 
-const { ArgumentParser } = require("argparse");
-const pjson = require("./package.json");
-const Motion = require("./lib/motion");
-const Camera = require("./lib/camera");
+const { ArgumentParser } = require('argparse');
+const pjson = require('./package.json');
+const Motion = require('./lib/motion');
+const Camera = require('./lib/camera');
 
 async function start(args) {
   const motion = new Motion({
@@ -28,34 +28,34 @@ async function start(args) {
 function main() {
   const parser = new ArgumentParser({
     addHelp: true,
-    description: "ONVIF motion detection events bridge to Motion",
+    description: 'ONVIF motion detection events bridge to Motion',
     version: pjson.version,
   });
 
-  parser.addArgument(["-m", "--motion-base-url"], {
-    help: "Base URL for the Motion instance (with trailing slash)",
+  parser.addArgument(['-m', '--motion-base-url'], {
+    help: 'Base URL for the Motion instance (with trailing slash)',
     required: true,
   });
-  parser.addArgument(["-i", "--motion-camera-id"], {
-    help: "The ID of the camera in Motion",
+  parser.addArgument(['-i', '--motion-camera-id'], {
+    help: 'The ID of the camera in Motion',
     required: true,
   });
-  parser.addArgument(["-c", "--hostname"], {
-    help: "hostname/IP of the ONVIF camera",
+  parser.addArgument(['-c', '--hostname'], {
+    help: 'hostname/IP of the ONVIF camera',
     required: true,
   });
-  parser.addArgument(["-u", "--username"], {
-    help: "username for the ONVIF camera",
+  parser.addArgument(['-u', '--username'], {
+    help: 'username for the ONVIF camera',
   });
-  parser.addArgument(["-p", "--password"], {
-    help: "password for the ONVIF camera",
+  parser.addArgument(['-p', '--password'], {
+    help: 'password for the ONVIF camera',
   });
-  parser.addArgument(["-o", "--port"], {
-    help: "port for the ONVIF camera",
+  parser.addArgument(['-o', '--port'], {
+    help: 'port for the ONVIF camera',
   });
-  parser.addArgument(["-t", "--timeout"], {
+  parser.addArgument(['-t', '--timeout'], {
     help:
-      "timeout (in ms) between no motion in the Camera and trigger the end of event to Motion",
+      'timeout (in ms) between no motion in the Camera and trigger the end of event to Motion',
     defaultValue: 10000,
   });
   const args = parser.parseArgs();
